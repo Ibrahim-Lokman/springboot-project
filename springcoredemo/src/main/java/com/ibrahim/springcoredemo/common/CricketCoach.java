274fcc78aeb67ee1cd176270f20fd171eafea8a6
IBRAHIM-LOKMAN\ibrah <ibrahimlokman.bd@gmail.com>
2023-10-21 12:38:56 +0600

    Use @Primary annotation for make a components or beans default for injecting.

--- springcoredemo/src/main/java/com/ibrahim/springcoredemo/common/CricketCoach.java
+++ springcoredemo/src/main/java/com/ibrahim/springcoredemo/common/CricketCoach.java
@@ -1,8 +1,10 @@
 package com.ibrahim.springcoredemo.common;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+
 public class CricketCoach implements Coach {
 
     @Override
