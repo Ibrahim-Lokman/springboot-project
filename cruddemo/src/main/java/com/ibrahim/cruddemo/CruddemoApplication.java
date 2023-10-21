d3044c83784a8546dcc774095a18603877b7ef7c
IBRAHIM-LOKMAN\ibrah <ibrahimlokman.bd@gmail.com>
2023-10-21 20:03:16 +0600

    Initial commit for cruddemo

---
+++ cruddemo/src/main/java/com/ibrahim/cruddemo/CruddemoApplication.java
@@ -0,0 +1,25 @@
+package com.ibrahim.cruddemo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class CruddemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CruddemoApplication.class, args);
+	}
+
+
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args){
+		return runner-> {
+			System.out.println("Hello Ibrahim");
+		};
+	}
+
+}
