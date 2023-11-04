ad3d9054c562e0d078a11dc40c8ec1e4ddd714cd
IBRAHIM-LOKMAN\ibrah <ibrahimlokman.bd@gmail.com>
2023-11-04 15:15:58 +0600

    @RepositoryRestResource used for defining customised path.  Customised endpoint when using spring data rest and jpa repository together.
    "/members" instead of "/employees".
    
    Define page size.

--- springbootcruddemo/src/main/java/com/ibrahim/springboot/springbootcruddemo/repository/EmployeeRepository.java
+++ springbootcruddemo/src/main/java/com/ibrahim/springboot/springbootcruddemo/repository/EmployeeRepository.java
@@ -2,7 +2,9 @@ package com.ibrahim.springboot.springbootcruddemo.repository;
 
 import com.ibrahim.springboot.springbootcruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
     // Spring will  provide CRUD implementation for free.
     // Exposes basic methods
