85b45f4485e78090a9089265df5b92f0bd555760
IBRAHIM-LOKMAN\ibrah <ibrahimlokman.bd@gmail.com>
2023-11-04 13:08:25 +0600

    Used @PathVariable @PostMapping @RequestBody @PutMapping @DeleteMapping
    for CRUD operation.

--- springbootcruddemo/src/main/java/com/ibrahim/springboot/springbootcruddemo/dao/EmployeeDAOJpaImpl.java
+++ springbootcruddemo/src/main/java/com/ibrahim/springboot/springbootcruddemo/dao/EmployeeDAOJpaImpl.java
@@ -9,9 +9,10 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public class EmployeeDAOJpaImpl implements EmployeeDAO{
+public class EmployeeDAOJpaImpl implements EmployeeDAO {
 
     private EntityManager entityManager;
+
     @Autowired
     public EmployeeDAOJpaImpl(EntityManager theEntityManager) {
         entityManager = theEntityManager;
@@ -22,5 +23,24 @@ public class EmployeeDAOJpaImpl implements EmployeeDAO{
         TypedQuery<Employee> theQuery = entityManager.createQuery("from Employee", Employee.class);
         List<Employee> employees = theQuery.getResultList();
         return employees;
+    }
+
+    @Override
+    public Employee findById(int theId) {
+        Employee theEmployee = entityManager.find(Employee.class, theId);
+        return theEmployee;
+    }
+
+    @Override
+    public Employee save(Employee theEmployee) {
+        Employee dbEmployee = entityManager.merge(theEmployee);
+
+        return dbEmployee;
+    }
+
+    @Override
+    public void deleteById(int theId) {
+        Employee theEmployee = entityManager.find(Employee.class, theId);
+        entityManager.remove(theEmployee);
     }
 }
