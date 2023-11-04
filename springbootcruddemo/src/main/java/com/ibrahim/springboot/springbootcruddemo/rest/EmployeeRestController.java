b230808407ab66834d5724549c00c0b2e8ae9fd1
IBRAHIM-LOKMAN\ibrah <ibrahimlokman.bd@gmail.com>
2023-11-04 15:05:25 +0600

    Spring Data REST entry on pom.xml.
    Disabled controller and service packages.

--- springbootcruddemo/src/main/java/com/ibrahim/springboot/springbootcruddemo/rest/EmployeeRestController.java
+++ springbootcruddemo/src/main/java/com/ibrahim/springboot/springbootcruddemo/rest/EmployeeRestController.java
@@ -1,64 +1,64 @@
-package com.ibrahim.springboot.springbootcruddemo.rest;
-
-//import com.ibrahim.springboot.springbootcruddemo.dao.EmployeeDAO;
-
-import com.ibrahim.springboot.springbootcruddemo.entity.Employee;
-import com.ibrahim.springboot.springbootcruddemo.service.EmployeeService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-
-@RestController
-@RequestMapping("/api")
-public class EmployeeRestController {
-
-    private EmployeeService employeeService;
-
-    @Autowired
-    public EmployeeRestController(EmployeeService theEmployeeService) {
-        employeeService = theEmployeeService;
-    }
-
-    @GetMapping("/employees")
-    public List<Employee> findAll() {
-        return employeeService.findAll();
-    }
-
-    @GetMapping("/employees/{employeeId}")
-    public Employee getEmployee(@PathVariable int employeeId) {
-        Employee theEmployee = employeeService.findById(employeeId);
-        if (theEmployee == null) {
-            throw new RuntimeException("Employee id not found - " + employeeId);
-
-        }
-        return theEmployee;
-    }
-
-    @PostMapping("/employees")
-    public Employee addEmployee(@RequestBody Employee theEmployee) {
-        theEmployee.setId(0);
-        Employee dbEmployee = employeeService.save(theEmployee);
-
-        return dbEmployee;
-    }
-
-    @PutMapping("/employees")
-    public Employee updateEmployee(@RequestBody Employee theEmployee) {
-
-        Employee dbEmployee = employeeService.save(theEmployee);
-
-        return dbEmployee;
-    }
-
-    @DeleteMapping("/employees/{employeeId}")
-    public String deleteEmployee(@PathVariable int employeeId) {
-        Employee tempEmployee = employeeService.findById(employeeId);
-        if (tempEmployee == null) {
-            throw new RuntimeException("Employee id not found - " + employeeId);
-        }
-        employeeService.deleteById(employeeId);
-        return "Deleted employee id - " + employeeId;
-    }
-
-}
+//package com.ibrahim.springboot.springbootcruddemo.rest;
+//
+////import com.ibrahim.springboot.springbootcruddemo.dao.EmployeeDAO;
+//
+//import com.ibrahim.springboot.springbootcruddemo.entity.Employee;
+//import com.ibrahim.springboot.springbootcruddemo.service.EmployeeService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.*;
+//
+//import java.util.List;
+//
+//@RestController
+//@RequestMapping("/api")
+//public class EmployeeRestController {
+//
+//    private EmployeeService employeeService;
+//
+//    @Autowired
+//    public EmployeeRestController(EmployeeService theEmployeeService) {
+//        employeeService = theEmployeeService;
+//    }
+//
+//    @GetMapping("/employees")
+//    public List<Employee> findAll() {
+//        return employeeService.findAll();
+//    }
+//
+//    @GetMapping("/employees/{employeeId}")
+//    public Employee getEmployee(@PathVariable int employeeId) {
+//        Employee theEmployee = employeeService.findById(employeeId);
+//        if (theEmployee == null) {
+//            throw new RuntimeException("Employee id not found - " + employeeId);
+//
+//        }
+//        return theEmployee;
+//    }
+//
+//    @PostMapping("/employees")
+//    public Employee addEmployee(@RequestBody Employee theEmployee) {
+//        theEmployee.setId(0);
+//        Employee dbEmployee = employeeService.save(theEmployee);
+//
+//        return dbEmployee;
+//    }
+//
+//    @PutMapping("/employees")
+//    public Employee updateEmployee(@RequestBody Employee theEmployee) {
+//
+//        Employee dbEmployee = employeeService.save(theEmployee);
+//
+//        return dbEmployee;
+//    }
+//
+//    @DeleteMapping("/employees/{employeeId}")
+//    public String deleteEmployee(@PathVariable int employeeId) {
+//        Employee tempEmployee = employeeService.findById(employeeId);
+//        if (tempEmployee == null) {
+//            throw new RuntimeException("Employee id not found - " + employeeId);
+//        }
+//        employeeService.deleteById(employeeId);
+//        return "Deleted employee id - " + employeeId;
+//    }
+//
+//}
--- springbootcruddemo/src/main/java/com/ibrahim/springboot/springbootcruddemo/service/EmployeeServiceImpl.java
+++ springbootcruddemo/src/main/java/com/ibrahim/springboot/springbootcruddemo/service/EmployeeServiceImpl.java
@@ -1,55 +1,55 @@
-package com.ibrahim.springboot.springbootcruddemo.service;
-
-//import com.ibrahim.springboot.springbootcruddemo.dao.EmployeeDAO;
-
-import com.ibrahim.springboot.springbootcruddemo.entity.Employee;
-import com.ibrahim.springboot.springbootcruddemo.repository.EmployeeRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
-import java.util.Optional;
-
-@Service
-public class EmployeeServiceImpl implements EmployeeService {
-    private EmployeeRepository employeeRepository;
-
-    @Autowired
-    public EmployeeServiceImpl(EmployeeRepository theEmployeeRepository) {
-        employeeRepository = theEmployeeRepository;
-    }
-
-    @Override
-    public List<Employee> findAll() {
-        return employeeRepository.findAll();
-    }
-
-    @Override
-    public Employee findById(int theId) {
-        Optional<Employee> result = employeeRepository.findById(theId);
-        Employee theEmployee = null;
-        if (result.isPresent()) {
-            theEmployee = result.get();
-        } else {
-            throw new RuntimeException("Did not find employee id - " + theId);
-        }
-        return theEmployee;
-    }
-
-    //JpaRepository provides transactional functionality
-    //@Transactional
-    @Override
-    public Employee save(Employee theEmployee) {
-
-        return employeeRepository.save(theEmployee);
-    }
-
-    //@Transactional
-    @Override
-    public void deleteById(int theId) {
-        employeeRepository.deleteById(theId);
-        ;
-    }
-
-}
+//package com.ibrahim.springboot.springbootcruddemo.service;
+//
+////import com.ibrahim.springboot.springbootcruddemo.dao.EmployeeDAO;
+//
+//import com.ibrahim.springboot.springbootcruddemo.entity.Employee;
+//import com.ibrahim.springboot.springbootcruddemo.repository.EmployeeRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//import java.util.List;
+//import java.util.Optional;
+//
+//@Service
+//public class EmployeeServiceImpl implements EmployeeService {
+//    private EmployeeRepository employeeRepository;
+//
+//    @Autowired
+//    public EmployeeServiceImpl(EmployeeRepository theEmployeeRepository) {
+//        employeeRepository = theEmployeeRepository;
+//    }
+//
+//    @Override
+//    public List<Employee> findAll() {
+//        return employeeRepository.findAll();
+//    }
+//
+//    @Override
+//    public Employee findById(int theId) {
+//        Optional<Employee> result = employeeRepository.findById(theId);
+//        Employee theEmployee = null;
+//        if (result.isPresent()) {
+//            theEmployee = result.get();
+//        } else {
+//            throw new RuntimeException("Did not find employee id - " + theId);
+//        }
+//        return theEmployee;
+//    }
+//
+//    //JpaRepository provides transactional functionality
+//    //@Transactional
+//    @Override
+//    public Employee save(Employee theEmployee) {
+//
+//        return employeeRepository.save(theEmployee);
+//    }
+//
+//    //@Transactional
+//    @Override
+//    public void deleteById(int theId) {
+//        employeeRepository.deleteById(theId);
+//        ;
+//    }
+//
+//}
