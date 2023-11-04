4532d440748afa29d775854cd6836c1c26db3814
IBRAHIM-LOKMAN\ibrah <ibrahimlokman.bd@gmail.com>
2023-11-04 14:23:37 +0600

    Spring Data Jpa. JpaRepository interface. Disabled code of DAO. Basic CRUD Operation with JpaRepository interface.

---
+++ springbootcruddemo/src/main/java/com/ibrahim/springboot/springbootcruddemo/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.ibrahim.springboot.springbootcruddemo.repository;
+
+import com.ibrahim.springboot.springbootcruddemo.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+    // Spring will  provide CRUD implementation for free.
+    // Exposes basic methods
+    // We just need to provide Entity class name and Primary key type.
+}
--- springbootcruddemo/src/main/java/com/ibrahim/springboot/springbootcruddemo/rest/EmployeeRestController.java
+++ springbootcruddemo/src/main/java/com/ibrahim/springboot/springbootcruddemo/rest/EmployeeRestController.java
@@ -1,6 +1,7 @@
 package com.ibrahim.springboot.springbootcruddemo.rest;
 
-import com.ibrahim.springboot.springbootcruddemo.dao.EmployeeDAO;
+//import com.ibrahim.springboot.springbootcruddemo.dao.EmployeeDAO;
+
 import com.ibrahim.springboot.springbootcruddemo.entity.Employee;
 import com.ibrahim.springboot.springbootcruddemo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
