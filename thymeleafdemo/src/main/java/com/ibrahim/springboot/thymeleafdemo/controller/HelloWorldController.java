7126b807384b00f00c82e2ce8b0c9a3545d8f402
IBRAHIM-LOKMAN\ibrah <ibrahimlokman.bd@gmail.com>
2023-11-04 20:17:15 +0600

    used HttpServletRequest, Added data to spring model. View page accesed data from the model.
    on v170.

--- thymeleafdemo/src/main/java/com/ibrahim/springboot/thymeleafdemo/controller/HelloWorldController.java
+++ thymeleafdemo/src/main/java/com/ibrahim/springboot/thymeleafdemo/controller/HelloWorldController.java
@@ -1,6 +1,8 @@
 package com.ibrahim.springboot.thymeleafdemo.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -13,6 +15,17 @@ public class HelloWorldController {
 
     @RequestMapping("/processForm")
     public String processForm(){
+        return "helloworld";
+    }
+
+    @RequestMapping("/processFormVersionTwo")
+    public String makeUppercase(HttpServletRequest request, Model model) {
+
+        String theName = request.getParameter("studentName");
+        theName = theName.toUpperCase();
+        String result = "Yo! "+theName;
+        model.addAttribute("message", result);
+        model.addAttribute("theDate", new java.util.Date());
         return "helloworld";
     }
 
