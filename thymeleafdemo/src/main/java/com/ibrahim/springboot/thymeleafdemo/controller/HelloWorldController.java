35f93567023f524a22752bc4b5ef516db7f1237a
IBRAHIM-LOKMAN\ibrah <ibrahimlokman.bd@gmail.com>
2023-11-10 01:02:38 +0600

    Uses of @RequestParam annotation.
    HttpServletRequest request,
    request.getParameter("studentName");
    
    replaced by @RequestParam("studentName") String theName.

--- thymeleafdemo/src/main/java/com/ibrahim/springboot/thymeleafdemo/controller/HelloWorldController.java
+++ thymeleafdemo/src/main/java/com/ibrahim/springboot/thymeleafdemo/controller/HelloWorldController.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
@@ -19,11 +20,21 @@ public class HelloWorldController {
     }
 
     @RequestMapping("/processFormVersionTwo")
-    public String makeUppercase(HttpServletRequest request, Model model) {
+    public String processFormV2(HttpServletRequest request, Model model) {
 
         String theName = request.getParameter("studentName");
         theName = theName.toUpperCase();
         String result = "Yo! "+theName;
+        model.addAttribute("message", result);
+        model.addAttribute("theDate", new java.util.Date());
+        return "helloworld";
+    }
+
+    @RequestMapping("/processFormVersionThree")
+    public String processFormV3(@RequestParam("studentName") String theName, Model model) {
+
+        theName = theName.toUpperCase();
+        String result = "Yo processFormV3! "+theName;
         model.addAttribute("message", result);
         model.addAttribute("theDate", new java.util.Date());
         return "helloworld";
