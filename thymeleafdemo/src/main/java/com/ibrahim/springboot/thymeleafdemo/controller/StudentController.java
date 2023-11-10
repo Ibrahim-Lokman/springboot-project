a70a9b156d5aed593bff30fde9db89461167f0eb
IBRAHIM-LOKMAN\ibrah <ibrahimlokman.bd@gmail.com>
2023-11-10 15:07:46 +0600

    Dropdown with Spring mvc form.
    Radio button without spring mvc form functionality.

---
+++ thymeleafdemo/src/main/java/com/ibrahim/springboot/thymeleafdemo/controller/StudentController.java
@@ -0,0 +1,33 @@
+package com.ibrahim.springboot.thymeleafdemo.controller;
+
+import com.ibrahim.springboot.thymeleafdemo.model.Student;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+public class StudentController {
+
+    @Value("${countries}")
+    private List<String> countries;
+    @GetMapping("/showStudentForm")
+    public String showForm(Model theModel) {
+        Student theStudent = new Student();
+        theModel.addAttribute("student", theStudent);
+        theModel.addAttribute("countries", countries);
+        return  "student-form";
+    }
+    @PostMapping("/processStudentForm")
+    public String processStudentForm(@ModelAttribute("student") Student thestudent) {
+
+
+        return "student-confirmation";
+    }
+
+}
