a3a804ef8f448f753b14d6ac64c7ffd2f70f6a79
IBRAHIM-LOKMAN\ibrah <ibrahimlokman.bd@gmail.com>
2023-11-10 11:14:17 +0600

    @RequestMapping(path: '/processedForm', method = RequesMethod.GET )
    ===== @GetMapping("processedForm")
    
    ===== @PostMapping

--- thymeleafdemo/src/main/java/com/ibrahim/springboot/thymeleafdemo/controller/HelloWorldController.java
+++ thymeleafdemo/src/main/java/com/ibrahim/springboot/thymeleafdemo/controller/HelloWorldController.java
@@ -3,13 +3,15 @@ package com.ibrahim.springboot.thymeleafdemo.controller;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
 
-    @RequestMapping("/showForm")
+    @GetMapping("/showForm")
     public String showForm(){
         return "helloworld-form";
     }
@@ -30,11 +32,21 @@ public class HelloWorldController {
         return "helloworld";
     }
 
-    @RequestMapping("/processFormVersionThree")
+    @GetMapping("/processFormVersionThree")
     public String processFormV3(@RequestParam("studentName") String theName, Model model) {
 
         theName = theName.toUpperCase();
-        String result = "Yo processFormV3! "+theName;
+        String result = "Yo processFormV3.2! "+theName;
+        model.addAttribute("message", result);
+        model.addAttribute("theDate", new java.util.Date());
+        return "helloworld";
+    }
+
+    @PostMapping("/processFormVersionFour")
+    public String processFormV4(@RequestParam("studentID") String theID, Model model) {
+
+
+        String result = "Id of student is ! "+theID;
         model.addAttribute("message", result);
         model.addAttribute("theDate", new java.util.Date());
         return "helloworld";
