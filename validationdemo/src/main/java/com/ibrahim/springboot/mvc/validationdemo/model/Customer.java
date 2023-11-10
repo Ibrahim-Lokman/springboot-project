d91ce4a0a57abff1a622c02030684bf153e0bd1f
IBRAHIM-LOKMAN\ibrah <ibrahimlokman.bd@gmail.com>
2023-11-10 20:39:27 +0600

    Validate with regexp.

--- validationdemo/src/main/java/com/ibrahim/springboot/mvc/validationdemo/model/Customer.java
+++ validationdemo/src/main/java/com/ibrahim/springboot/mvc/validationdemo/model/Customer.java
@@ -1,15 +1,22 @@
 package com.ibrahim.springboot.mvc.validationdemo.model;
 
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 public class Customer {
 
 
     @NotNull(message = "is required")
-    @Size(min=2, message = "Atleast 2 character needed")
+    @Size(min=2, message = "At least 2 character needed")
     private String firstName = "" ;
     private String lastName;
+
+    @NotNull(message = "is required")
+    @Min(value =  18, message =  "must be >= 18")
+    @Max(value =  40, message =  "must be <= 35")
+    private int ageLimit;
+
+    @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "Enter 5 chars/digits")
+    private String postalCode;
 
     public String getFirstName() {
         return firstName;
@@ -25,5 +32,21 @@ public class Customer {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public int getAgeLimit() {
+        return ageLimit;
+    }
+
+    public void setAgeLimit(int ageLimit) {
+        this.ageLimit = ageLimit;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
     }
 }
